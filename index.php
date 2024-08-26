6dc40a01fee48c8e3f273c7ea7c30393c82f2acc
NathanMad <nathan@internit.com.br>
2024-08-26 03:38:52 -0300

    edit index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once __DIR__. '/controller/Controller.php';
+require_once __DIR__ . '/controller/Controller.php';
 
 use function Controller\createCarne;
 use function Controller\findCarne;
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        if(isset($_GET['id'])) {
-            if(findCarne($_GET['id'])) {
+        if (isset($_GET['id'])) {
+            if (findCarne($_GET['id'])) {
                 echo findCarne($_GET['id']);
             } else {
                 echo json_encode(["Status" => "Erro", "Mensagem" => "Não existe carnê com este id"]);
@@ -19,7 +19,11 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $data = file_get_contents("php://input");
         $data_assoc = json_decode($data, true);
         try {
-            $carne = createCarne($data_assoc['valor_total'], $data_assoc['qtd_parcelas'], $data_assoc['data-primeiro-vencimento'], $data_assoc['periodicidade']);
+            if (isset($data_assoc['entrada'])) {
+                $carne = createCarne($data_assoc['valor_total'], $data_assoc['qtd_parcelas'], $data_assoc['data-primeiro-vencimento'], $data_assoc['periodicidade'], $data_assoc['entrada']);
+            } else {
+                $carne = createCarne($data_assoc['valor_total'], $data_assoc['qtd_parcelas'], $data_assoc['data-primeiro-vencimento'], $data_assoc['periodicidade']);
+            }
             echo json_encode(["Status" => "Sucesso", "Carnê" => $carne]);
         } catch (\Throwable $th) {
             echo json_encode(["Status" => "Erro", "Mensagem" => "Erro na criação do carnê"]);
